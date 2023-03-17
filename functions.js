382ee5d588e8dbd5cb05ad85fd5c269f2628658f
saintskeeper <walterdayjr@gmail.com>
2023-03-17 19:10:14 -0400

    fixing json

---
+++ functions.js
@@ -0,0 +1,28 @@
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+const next = require("next");
+const config = require("./next.config");
+
+
+admin.initializeApp();
+
+const app = next({
+	// the absolute directory from the package.json file that initialises this module
+	// IE: the absolute path from the root of the Cloud Function
+	conf: config,
+});
+const handle = app.getRequestHandler();
+
+const server = functions
+.runWith({
+  minInstances: 1,
+  maxInstances: 3,
+  timeoutSeconds: 300,
+  memory: "1GB",
+})
+.https.onRequest((request, response) => {
+	// log the page.js file or resource being requested
+	return app.prepare().then(() => handle(request, response));
+});
+
+exports.nextjs = { server };
