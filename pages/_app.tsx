72035529cd6826ccea99bb0c34375cb807c07fac
Haruxe <haruxe@proton.me>
2023-03-18 01:47:52 -0700

    merged UI changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="WKaijuDex" key="ogtitle" />
+        <meta
+          property="og:description"
+          content="Finding work for kingz"
+          key="ogdesc"
+        />
+        <meta property="og:image" content="" key="ogimage" />
+        <title>KaijuDex</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
