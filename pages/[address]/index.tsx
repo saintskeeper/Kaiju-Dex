ce4f4fd56c2f6deb6c96bc0d601a2a195a412f32
Haruxe <haruxe@proton.me>
2023-03-20 17:15:38 -0700

    add profile page and wagmi stuff

---
+++ pages/[address]/index.tsx
@@ -0,0 +1,45 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import { Crown } from "styled-icons/boxicons-regular";
+import { useAccount, useEnsName } from "wagmi";
+import { useBalance } from "wagmi";
+
+const Home: NextPage = () => {
+  const { address, connector, isConnected } = useAccount();
+  const {
+    data: ens,
+    isError: ensError,
+    isLoading: ensLoading,
+  } = useEnsName({ address: address });
+  return (
+    <>
+      <div className="bg-[#2e3031] rounded-lg p-3 flex-col space-y-5 font-bold font-mono">
+        <div className="flex space-x-5">
+          <Image
+            height="200"
+            width="200"
+            src="/images/demo.gif"
+            className="rounded-xl"
+            alt="demo"
+          />
+          <div className="py-2 space-y-3 flex flex-col relative">
+            <h1 className="font-mono text-xl font-bold tracking-wider uppercase">
+              {ensLoading ? "..." : ensError ? address?.slice(0, 5) : ens}
+            </h1>
+            <p className="font-mono text-md text-gray-400 tracking-wider">
+              This is my bio! <br /> YOO!
+            </p>
+            <div className="flex space-x-2 absolute bottom-0">
+              <Crown className="w-5" />
+              <p className="font-mono text-md text-gray-200 tracking-wider">
+                03.22.22
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { WagmiConfig, createClient } from "wagmi";
+import { WagmiConfig, createClient, useBalance } from "wagmi";
 import { configureChains, mainnet } from "@wagmi/core";
 import { publicProvider } from "@wagmi/core/providers/public";
 import {
@@ -10,11 +10,17 @@ import {
   darkTheme,
 } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
+import { PersonFill } from "styled-icons/bootstrap";
+import { SearchAlt } from "styled-icons/boxicons-regular";
+import { CatchingPokemon, Settings } from "styled-icons/material-twotone";
+import { useAccount } from "wagmi";
 
 const { provider, chains } = configureChains([mainnet], [publicProvider()]);
 
 const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
+  appName: "KaijuDex",
   chains,
 });
 
@@ -24,14 +30,19 @@ const wagmiClient = createClient({
   provider,
 });
 
+const kaijuContract = "0x...";
+const scientistContract = "0x...";
+const mutantContract = "0x...";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const { address, connector, isConnected } = useAccount();
   return (
-    <>
+    <div className="bg-[#262829]">
       <Head>
         <meta property="og:title" content="WKaijuDex" key="ogtitle" />
         <meta
           property="og:description"
-          content="Finding work for kingz"
+          content="Finding work for Kingz"
           key="ogdesc"
         />
         <meta property="og:image" content="" key="ogimage" />
@@ -49,10 +60,54 @@ function MyApp({ Component, pageProps }: AppProps) {
             overlayBlur: "small",
           })}
         >
-          <Component {...pageProps} />
+          <div className="place-items-center max-w-[1400px] mx-auto">
+            <div className="flex">
+              <div className="bg-[#2e3031] w-[300px] hidden md:flex h-screen text-white p-5 place-content-center">
+                <div className="flex space-x-2 mb-auto">
+                  <CatchingPokemon className="w-7 animate-spin" />
+                  <Link href="/">
+                    <h1 className="font-mono text-2xl font-bold tracking-wider hover:scale-105 cursor-pointer active:scale-95 duration-100">
+                      KAIJUDEX
+                    </h1>
+                  </Link>
+                </div>
+              </div>
+              <div className="bg-[#202124] w-full h-screen text-white p-5 space-y-5">
+                <div className="flex space-x-3 place-items-center">
+                  <div className="w-[20rem] bg-[#2e3031] h-8 rounded-full py-1 px-3 mr-auto flex">
+                    <SearchAlt className="w-5 my-auto" />
+                    <input
+                      type={"text"}
+                      placeholder="Search"
+                      className="w-[85%] mx-auto flex bg-transparent border-none outline-none font-sans text-sm"
+                    ></input>
+                  </div>
+                  <div className="bg-[#1A1B1F] hover:scale-110 p-2 font-sans font-bold rounded-full flex hover:cursor-pointer duration-100 active:scale-95 place-content-center space-x-1">
+                    <Settings className="w-5 mx-auto my-auto" />
+                  </div>
+                  <div className="bg-[#1A1B1F] hover:scale-110 p-2 font-sans font-bold rounded-full flex hover:cursor-pointer duration-100 active:scale-95 place-content-center space-x-1">
+                    <Link
+                      href={"/" + address}
+                      className="w-5 mx-auto my-auto flex"
+                    >
+                      <PersonFill />
+                    </Link>
+                  </div>
+                  <ConnectButton showBalance={false} />
+                </div>
+                {isConnected ? (
+                  <Component {...pageProps} />
+                ) : (
+                  <h1 className="font-mono text-xl">
+                    Connect Wallet For Access
+                  </h1>
+                )}
+              </div>
+            </div>
+          </div>
         </RainbowKitProvider>
       </WagmiConfig>
-    </>
+    </div>
   );
 }
 
