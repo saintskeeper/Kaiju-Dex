7aa1ce13ebc2383482f5552890dc65547dec901d
Haruxe <haruxe@proton.me>
2023-03-25 18:23:17 -0700

    further styling and prep

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -3,20 +3,22 @@ import { RightArrow } from "styled-icons/boxicons-solid";
 
 function FeatureModal(props: any) {
   return (
-    <div className="bg-[#2e3031] rounded-lg p-3 flex-col space-y-5 font-bold">
+    <div className="bg-[#24242494] backdrop-blur-xl rounded-lg p-5 flex-col space-y-5 font-bold mb-5">
       <div>
         <h1 className="text-xl font-bold tracking-wider uppercase">
           {props.title}
         </h1>
       </div>
-      <div className="flex space-x-5">{props.children}</div>
+      <div className="flex flex-row scroll-smooth overflow-scroll space-x-5 p-5 snap-x">
+        {props.children}
+      </div>
     </div>
   );
 }
 
 const Profile = () => {
   return (
-    <div className="relative">
+    <div className="relative snap-start">
       <div className="w-[180px] h-[180px] bg-[#7c7b7b4f] rounded-xl animate-pulse duration-500"></div>
       <div className="z-10 absolute bottom-0 flex w-full p-1 space-x-1 h-12 text-sm">
         <div className="flex backdrop-blur-lg bg-[#0000009f] rounded-xl w-full py-1 px-2">
@@ -34,7 +36,44 @@ const Profile = () => {
 const Home: NextPage = () => {
   return (
     <>
-      <FeatureModal title="Featured">
+      <FeatureModal title="Featured artists">
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+      </FeatureModal>
+      <FeatureModal title="Featured engineers">
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
         <Profile />
       </FeatureModal>
     </>
