d80f4d7a48481f96d1ba2b251f05ef7c8844a1a2
saintskeeper <walterdayjr@gmail.com>
2023-04-13 23:39:22 -0400

    adding this weeks article

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -9,9 +9,32 @@ const markdownContent = `
 Kaiju Creators wanted
 `;
 
+const markdownContent2 = `
+### Builders Be Building
+Artists spotlight community initiatives!
+`;
+
 const Home: NextPage = () => {
   return (
-    <div>
+    <div >
+      <Link
+        href="/news/articles/article2"
+        className="cursor-pointer flex w-[600px] md:w-[600px] "
+      >
+        <div className="space-y-1 mr-auto outline outline-2 outline-zinc-300 rounded-md hover:scale-105 duration-200 flex flex-col">
+          <div className="relative h-[250px] w-[250px]">
+            <Image
+              src="/images/article2/Kaiju-Dev-High-Quality-01.png"
+              alt="kaijudev"
+              fill
+              className="rounded-md"
+            />
+          </div>
+          <div className="p-5 space-y-5 ">
+            <MarkdownArticle markdown={markdownContent2} />
+          </div>
+        </div>
+      </Link>
       <Link
         href="/news/articles/article1"
         className="cursor-pointer flex w-[600px] md:w-[600px] "
