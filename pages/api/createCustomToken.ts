1a618fd424fa825c853f998ee0862f5409952d14
saintskeeper <walterdayjr@gmail.com>
2023-04-15 21:07:16 -0400

    adding rest of todays work

--- pages/api/createCustomToken.ts
+++ pages/api/createCustomToken.ts
@@ -1,14 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import * as admin from 'firebase-admin';
 
+const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+if (!firebasePrivateKey) {
+  throw new Error("FIREBASE_PRIVATE_KEY environment variable is missing!");
+}
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      //ts ignore is needed because the private key is a string with new lines
+      // @ts-ignore
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+
     }),
   });
+}
+
+async function createOrUpdateUserDocument(ethereumAddress: string, customToken: string) {
+  const userRef = admin.firestore().collection("users").doc(ethereumAddress);
+
+  const userDoc = await userRef.get();
+  if (userDoc.exists) {
+    // Update the existing document with the new custom token
+    await userRef.update({ customToken: customToken });
+  } else {
+    // Create a new document with the Ethereum address and custom token
+    await userRef.set({ customToken: customToken });
+  }
 }
 
 async function createCustomToken(uid: string) {
@@ -33,7 +55,12 @@ export default async function handler(
     }
 
     try {
+      // Create the custom token
       const customToken = await createCustomToken(address);
+
+      // Create or update the user document in Firestore
+      await createOrUpdateUserDocument(address, customToken);
+
       return res.status(200).json({ customToken });
     } catch (error) {
       return res.status(500).json({ error: 'Error creating custom token' });
